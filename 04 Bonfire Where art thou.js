aadb94ae9c3b2414a742747304ad0e48329c1ef9
Vitaly Doroshenko <doroshenko74@gmail.com>
2015-12-15 19:18:17 +0200

    Bonfire: Where art thou

---
+++ 04 Bonfire Where art thou.js
@@ -0,0 +1,34 @@
+/*Bonfire: Where art thou
+Make a function that looks through an array of objects (first argument) and returns an array of all objects that have matching property and value pairs (second argument). Each property and value pair of the source object has to be present in the object from the collection if it is to be included in the returned array.
+
+For example, if the first argument is [{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], and the second argument is { last: "Capulet" }, then you must return the third object from the array (the first argument), because it contains the property and it's value, that was passed on as the second argument.
+
+Remember to use Read-Search-Ask if you get stuck. Write your own code.
+
+Here are some helpful links:
+
+Global Object
+Object.hasOwnProperty()
+Object.keys()*/
+
+function where(collection, source) {
+    var prop = Object.keys(source);
+    return collection.filter(function (item) {
+        return prop.every(function (prop) {
+            return item[prop] === source[prop];
+        });
+    });
+}
+
+where([{
+    first: "Romeo",
+    last: "Montague"
+}, {
+    first: "Mercutio",
+    last: null
+}, {
+    first: "Tybalt",
+    last: "Capulet"
+}], {
+    last: "Capulet"
+});
